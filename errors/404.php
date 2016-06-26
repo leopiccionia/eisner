677bf23654f569d0278f7dc62ee6015d5c994bea
Leonardo Piccioni de Almeida <leopiccionia@gmail.com>
2016-06-26 19:37:21 +0000

    Error 404 page.

---
+++ errors/404.php
@@ -0,0 +1,15 @@
+<?php
+	/* TODO: Embelishment */
+	/* TODO: More useful messages */
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Eisner :: Error 404</title>
+		<meta charset="UTF-8" />
+	</head>
+	<body>
+		<h1>Error 404</h1>
+		<p>Sorry, we couldn't locate the content you're looking for. It could have been moved or removed.</p>
+	</body>
+</html>
