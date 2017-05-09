4325ea0f2425bdf1f2e05c08752222436312c95a
leopiccionia <leopiccionia@gmail.com>
2017-05-08 23:46:19 -0300

    Some cleaning...

--- imports/ui/AppMenu.vue
+++ imports/ui/AppMenu.vue
@@ -11,23 +11,6 @@ nav.navbar.navbar-default.navbar-static-top(placement='top')
 				router-link(to='add') Add
 			li
 				router-link(to='demo') Demo
-//	<nav class="navbar navbar-default navbar-static-top" placement="top">
-//		<div class="navbar-header">
-//			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-menu-collapse" aria-expanded="false">
-//				<i class="glyphicon glyphicon-menu-hamburger"></i>
-//			</button>
-//			<router-link class="navbar-brand" to="/">
-//				<img src="/img/icon_transparent.svg"/>
-//			</router-link>
-//		</div>
-//
-//		<div id="app-menu-collapse" class="collapse navbar-collapse">
-//			<ul class="nav navbar-nav">
-//				<li><router-link to="add">Add</router-link></li>
-//				<li><router-link to="demo">Demo</router-link></li>
-//			</ul>
-//		</div>
-//	</nav>
 </template>
 
 <script>
